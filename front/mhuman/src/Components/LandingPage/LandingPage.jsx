190a3260d86e7e504fd86acdb89849238e9c6666
Aline Massard <massard.aline@gmail.com>
2022-10-27 16:09:10 +0200

    Set up React

---
+++ front/mhuman/src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './landingpagestyles.scss'
+
+function LandingPage() {
+    return (
+        <h1>Hi! I'm the LandingPage!</h1>
+    )
+}
+
+export default React.memo(LandingPage);
