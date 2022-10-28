527007c4faeebc6c2fc79cf4b16c9435c71844c1
romain-pfeiffer <romainpfeiffer78@gmail.com>
2022-10-28 09:48:03 +0200

    [SignUp]add Form and make controlled Input

--- front/mhuman/src/Components/SignUp/SignUp.jsx
+++ front/mhuman/src/Components/SignUp/SignUp.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form } from 'semantic-ui-react'
 import './signupstyles.scss'
 
 function SignUp() {
+    const [emailValue, SetEmailValue] = useState('');
+    const [passwordValue, SetPasswordValue] = useState('');
+    const [confirmPasswordValue, SetConfirmPasswordValue] = useState('');
     return (
-        <h1>Hi! I'm the SignUpPage!</h1>
+        <Form>
+        <Form.Field width={3}>
+          <input 
+            placeholder='Email'
+            value={emailValue}
+            onChange={(e)=>{SetEmailValue(e.target.value)}}
+          />
+        </Form.Field>
+        <Form.Field width={3}>
+          <input
+            placeholder='Password'
+            value={passwordValue}
+            onChange={(e)=>{SetPasswordValue(e.target.value)}}
+          />
+        </Form.Field>
+        <Form.Field width={3}>
+          <input
+            placeholder='Confirm password'
+            value={confirmPasswordValue}
+            onChange={(e)=>{SetConfirmPasswordValue(e.target.value)}}
+          />
+        </Form.Field>
+        <Button type='submit'>Valider</Button>
+      </Form>
     )
 }
 
