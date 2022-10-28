cb740659bad3321c0df04aa12d626d29559f3705
Aline Massard <massard.aline@gmail.com>
2022-10-28 11:38:47 +0200

    [set]-Landing Page

--- front/mhuman/src/App.js
+++ front/mhuman/src/App.js
@@ -1,15 +1,12 @@
 import LandingPage from './Components/LandingPage/LandingPage';
 import 'semantic-ui-css/semantic.min.css'
 import './App.scss';
-import LogIn from './Components/LogIn/LogIn';
-import SignUp from './Components/SignUp/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
-      <LogIn />
-      <SignUp />
+        <LandingPage />
+        
     </div>
   );
 }
