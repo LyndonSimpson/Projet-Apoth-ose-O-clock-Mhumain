39ed82db13cd4ee5effcbcd59c2fa0f3c13262d5
LyndonSimpson <lyndon.simpson@gmail.com>
2022-10-29 12:06:47 +0200

    terminé le CRUD user

--- API/app/datamapper/user.js
+++ API/app/datamapper/user.js
@@ -1,52 +1,53 @@
 const database = require('../../data/database');
 
 const userDattaMapper = {
-    async createUser(email, password) { 
+  
+  async createUser(email, password) {
 
-        const query = {
-            text: `INSERT INTO account(email, password) 
+    const query = {
+      text: `INSERT INTO account(email, password) 
             VALUES($1,$2)`,
-            values: [email, password]
-          };
-        const result = await database.query(query);
-        return result.rows;
-    },
-    async getUserById(id) { 
-
-      const query = {
-          text: `SELECT * FROM account WHERE id = $1`,
-          values: [id]
-        };
-      const result = await database.query(query);
-      return result.rows;
+      values: [email, password]
+    };
+    const result = await database.query(query);
+    return result.rows;
   },
-    async getUsers() { 
+  async getUserById(id) {
 
-      const query =`SELECT * FROM account;`;
-
-      const result = await database.query(query);
-      return result.rows;
+    const query = {
+      text: `SELECT * FROM account WHERE id = $1`,
+      values: [id]
+    };
+    const result = await database.query(query);
+    return result.rows;
   },
-    async updateUser(id, email, password) { 
+  async getUsers() {
 
-        const query = {
-            text: `UPDATE account
+    const query = `SELECT * FROM account;`;
+
+    const result = await database.query(query);
+    return result.rows;
+  },
+  async updateUser(id, email, password) {
+
+    const query = {
+      text: `UPDATE account
             SET email = $1, password = $2
             WHERE id = $3`,
-            values: [email, password, id]
-          };
-        const result = await database.query(query);
-        return result.rows;
-    },
-    async deleteUser(id) { 
+      values: [email, password, id]
+    };
+    const result = await database.query(query);
+    return result.rows;
+  },
+  async deleteUser(id) {
 
-        const query = {
-            text: `DELETE FROM account WHERE id = $1`,
-            values: [id]
-          };
-        const result = await database.query(query);
-        return result.rows;
-    }
+    const query = {
+      text: `DELETE FROM account WHERE id = $1`,
+      values: [id]
+    };
+    const result = await database.query(query);
+    return result.rows;
+  }
 }
 
 module.exports = userDattaMapper;
