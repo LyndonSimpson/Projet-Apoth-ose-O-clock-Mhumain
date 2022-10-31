e5fdc207a47013d195063517a9eed749a14a33d5
romain-pfeiffer <romainpfeiffer78@gmail.com>
2022-10-31 16:22:09 +0100

    [Header and Footer]make Header

--- front/mhuman/src/App.jsx
+++ front/mhuman/src/App.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './App.scss';
+import AppHeader from './Components/AppHeader/AppHeader';
+import Footer from './Components/Footer/Footer';
 import LandingPage from './Components/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <LandingPage />
+          }
+        />
+        <Route
+          path="/home"
+          element={(
+            <>
+              <AppHeader />
+              <Footer />
+            </>
+  )}
+        />
+      </Routes>
     </div>
   );
 }
---
+++ front/mhuman/src/Components/AppHeader/AppHeader.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Icon } from 'semantic-ui-react';
+
+import './appheaderstyles.scss';
+
+function AppHeader() {
+  return (
+    <header className="header">
+      <img className="logo" src="logocouleur.png" alt="logo adopte ton mhumains" />
+      <div className="nav-icon">
+        <Link to="/search"><Icon name="search" size="big" /></Link>
+        <Link to="/fav"><Icon name="heart outline" size="big" /></Link>
+        <Link to="/profil"><Icon name="user" size="big" /></Link>
+      </div>
+    </header>
+  );
+}
+
+export default React.memo(AppHeader);
