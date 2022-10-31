cce232c95624e603266b9a9eeb6a1e11b055fc0d
HugoBnl <bonnelhugo@gmail.com>
2022-10-31 16:48:43 +0100

    non dynamyc done, miss api connection,fixe for merge?

--- front/mhuman/src/App.js
+++ front/mhuman/src/App.js
@@ -2,15 +2,10 @@ import 'semantic-ui-css/semantic.min.css'
 import './App.scss';
 import ProfileSelect from './Components/ProfileSelect/ProfileSelect';
 import LandingPage from './Components/LandingPage/LandingPage'
-import { Route , Routes } from 'react-router-dom'
 
 function App() {
   return (
-  <Routes>
-    <Route path="/" element={<LandingPage />} />
-    <Route path="/profileselect" element={<ProfileSelect />} />
-    <Route path="*" element={<p>not found bro</p>} />
-  </Routes>
+    <LandingPage />
   );
 }
 
