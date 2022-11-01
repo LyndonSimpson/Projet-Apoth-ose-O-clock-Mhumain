54a268bbc909e15432a90f0d4f4b2a18d4b749ee
HugoBnl <bonnelhugo@gmail.com>
2022-11-01 11:27:34 +0100

    fixed profileselect & eslint

--- front/mhuman/src/App.jsx
+++ front/mhuman/src/App.jsx
@@ -6,9 +6,9 @@ import './App.scss';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 
-import { Routes, Route } from 'react-router-dom';
 import LandingPage from './Components/LandingPage/LandingPage';
 import CreateProfile from './Components/CreateProfile/CreateProfile';
+import ProfileSelect from './Components/ProfileSelect/ProfileSelect';
 
 function App() {
   return (
@@ -16,9 +16,20 @@ function App() {
       <Routes>
         <Route
           path="/"
-
           element={
             <LandingPage />
+          }
+        />
+        <Route
+          path="/createprofile"
+          element={
+            <CreateProfile />
+          }
+        />
+        <Route
+          path="/profileselect"
+          element={
+            <ProfileSelect />
           }
         />
         <Route
--- front/mhuman/src/Components/ProfileSelect/AddProfile/AddProfile.jsx
+++ front/mhuman/src/Components/ProfileSelect/AddProfile/AddProfile.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
+import React from 'react';
 import { BsPlusLg } from 'react-icons/bs';
-import './addprofile.scss'
+import './addprofile.scss';
 
 function AddProfile() {
-    return (
-        <div className='AddProfile'>
-            <div className='AddProfilePlus'>
-                <BsPlusLg className='BsPlusLg' />
-            </div>
-            <p className='AddProfilName'>Ajouter un profil</p>
-        </div>
-    )
+  return (
+    <div className="AddProfile">
+      <div className="AddProfilePlus">
+        <BsPlusLg className="BsPlusLg" />
+      </div>
+      <p className="AddProfilName">Ajouter un profil</p>
+    </div>
+  );
 }
-
 
 export default React.memo(AddProfile);
