ce2e6d0e41727686c14f0e8b3ffb536680d23648
Aline Massard <massard.aline@gmail.com>
2022-11-01 11:50:56 +0100

    responsive form

--- front/mhuman/src/Components/CreateProfileHuman/FormHumanInformations/FormHumanInformations.jsx
+++ front/mhuman/src/Components/CreateProfileHuman/FormHumanInformations/FormHumanInformations.jsx
@@ -17,69 +17,75 @@ function FormHumanInformations() {
     <>
       {!next && (
         <div className="form-human">
-          <Form.Group widths="equal">
-            <Form.Field
+          <Form.Group className="form-informations">
+            <Input
+              className="form-informations-input"
               id="form-input-control-first-name"
               control={Input}
               placeholder="Name"
             />
-            <Form.Field
+            <Input
+              className="form-informations-input"
               id="form-input-control-last-name"
               control={Input}
               placeholder="Pseudo"
             />
             <Input
+              className="form-informations-input"
               label={{ basic: true, content: 'ans' }}
               labelPosition="right"
               placeholder="Entrez votre âge"
               type="number"
             />
           </Form.Group>
-          <Form.Group grouped>
-            <label htmlFor="has_pets">Avez-vous des animaux ?</label>
-            <Form.Field
-              label="Oui"
-              control="input"
-              type="radio"
-              name="has_pets"
-            />
-            <Form.Field
-              label="Non"
-              control="input"
-              type="radio"
-              name="has_pets"
-            />
-          </Form.Group>
-          <Form.Group grouped>
-            <label htmlFor="has_kids">Avez-vous des enfants ?</label>
-            <Form.Field
-              label="Oui"
-              control="input"
-              type="radio"
-              name="has_kids"
-            />
-            <Form.Field
-              label="Non"
-              control="input"
-              type="radio"
-              name="has_kids"
-            />
-          </Form.Group>
-          <Form.Group grouped>
-            <label htmlFor="has_garden">Avez-vous un jardin ?</label>
-            <Form.Field
-              label="Oui"
-              control="input"
-              type="radio"
-              name="has_garden"
-            />
-            <Form.Field
-              label="Non"
-              control="input"
-              type="radio"
-              name="has_garden"
-            />
-          </Form.Group>
+
+          <div className="form-informations-radios">
+            <Form.Group grouped>
+              <label htmlFor="has_pets">Avez-vous des animaux ?</label>
+              <Form.Field
+                label="Oui"
+                control="input"
+                type="radio"
+                name="has_pets"
+              />
+              <Form.Field
+                label="Non"
+                control="input"
+                type="radio"
+                name="has_pets"
+              />
+            </Form.Group>
+            <Form.Group grouped>
+              <label htmlFor="has_kids">Avez-vous des enfants ?</label>
+              <Form.Field
+                label="Oui"
+                control="input"
+                type="radio"
+                name="has_kids"
+              />
+              <Form.Field
+                label="Non"
+                control="input"
+                type="radio"
+                name="has_kids"
+              />
+            </Form.Group>
+            <Form.Group grouped>
+              <label htmlFor="has_garden">Avez-vous un jardin ?</label>
+              <Form.Field
+                label="Oui"
+                control="input"
+                type="radio"
+                name="has_garden"
+              />
+              <Form.Field
+                label="Non"
+                control="input"
+                type="radio"
+                name="has_garden"
+              />
+            </Form.Group>
+          </div>
           <div className="form-human-button">
             <Button
               onClick={handleFormButton}
