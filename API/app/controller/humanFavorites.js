d19a6f26d6a1a71b9eef1514834551c5dbe4b8cb
LyndonSimpson <lyndon.simpson@gmail.com>
2022-11-02 18:36:43 +0100

    req.session.cat/human in newFavorite method

--- API/app/controller/humanFavorites.js
+++ API/app/controller/humanFavorites.js
@@ -4,8 +4,8 @@ const dataMapper = require("../datamapper/humanFavorites");
 const humanFavoritesController = {
   newFavorite: async (req, res) => {
     try {
-      const result = await dataMapper.createFavorite(req.session.user.id, req.body.liked_profile_id); //todo  const { firstName, lastName, email, password } = req.body; this his how you do it
-      res.json(result);                                                   //TODO req.session.user.id ici ! --- le human connecté like depuis son id!
+      const result = await dataMapper.createFavorite(req.session.human.id, req.body.liked_profile_id); //todo  const { firstName, lastName, email, password } = req.body; this his how you do it
+      res.json(result);                                                   //TODO req.session.human.id ici ! --- le human connecté like depuis son id!
                                                                           //reste plus qu'à voir comment récupérer le liked_profile_id
     } catch (error) {
       console.error(error);
