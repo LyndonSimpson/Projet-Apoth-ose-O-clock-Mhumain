3569503f2233b86eaf5de5ba5ca483fbc5b7276f
LyndonSimpson <lyndon.simpson@gmail.com>
2022-11-07 16:31:39 +0100

    debug static files

--- API/app/router/cat.js
+++ API/app/router/cat.js
@@ -16,7 +16,7 @@ const router = Router();
     * @description inserts a new cat profile into the database
     * @param {string} request.body
     */
-    router.post("/cat", authorizationMiddleware, catController.newCat);
+    router.post("/cat", catController.newCat); //TODO ne pas utiliser cette route. pour le tests de multer.
     
     /**
     * POST /cat/signup
