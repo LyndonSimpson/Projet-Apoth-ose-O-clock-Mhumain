af51a270988f17df4e4f64df507c239a9679a8c5
LyndonSimpson <lyndon.simpson@gmail.com>
2022-11-08 14:59:30 +0100

    repair main

--- API/app/middlewares/storage.js
+++ API/app/middlewares/storage.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const multer = require('multer');
+//../../projet-04-adopte-ton-mhumain/front/mhuman/public
+//../../projet-04-adopte-ton-mhumain/API/public
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, '../../projet-04-adopte-ton-mhumain/API/public');
+        cb(null, '../../projet-04-adopte-ton-mhumain/front/mhuman/public');
     },
     filename: (req, file, cb) => {
         //console.log(file)
