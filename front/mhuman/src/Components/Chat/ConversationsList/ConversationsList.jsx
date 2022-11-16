6171ea5079be2c4ec948b7c8a62dba4bcb041f10
romain-pfeiffer <romainpfeiffer78@gmail.com>
2022-11-16 17:45:56 +0100

    Init conversationList

--- front/mhuman/src/Components/Chat/ConversationsList/ConversationsList.jsx
+++ front/mhuman/src/Components/Chat/ConversationsList/ConversationsList.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import { Image } from 'semantic-ui-react';
 import Footer from '../../Footer/Footer';
 import Header from '../../Header/Header';
 import './conversationsliststyles.scss';
 import MobileNav from '../../Header/MobileNav/MobileNav';
-import Cat from '../../../styles/cat.jpg';
+import { getCatConversationsRequest, getHumanConversationsRequest } from '../../../requests/messageRequests';
 
 function ConversationsList() {
+  const [conversations, setConversations] = useState([]);
   const type = localStorage.getItem('type');
+
+  React.useEffect(() => {
+    const getConversations = async () => {
+      if (type === 'cat') {
+        const response = await getCatConversationsRequest();
+        setConversations(response);
+      } else {
+        const response = await getHumanConversationsRequest();
+        setConversations('inComponent', response);
+      }
+    };
+    getConversations();
+  }, []);
+
   return (
     <div className="conversations-styles">
       <Header />
@@ -16,11 +32,20 @@ function ConversationsList() {
         className="conversations"
       >
         <h2 className="conversation-title">Mes conversations</h2>
-        <div className="conversation-item">
-          <Image className="conversation-image" src={Cat} size="tiny" rounded />
-          <p>Ma conversation avec Torchon</p>
-        </div>
-
+        {conversations && (
+          conversations.map(({ image, pseudo, id }) => (
+            <Link to="/chat" state={id} key={id}>
+              <div className="conversation-item">
+                <Image className="conversation-image" src={image} size="tiny" rounded />
+                <p>
+                  Ma conversation avec
+                  {' '}
+                  {pseudo}
+                </p>
+              </div>
+            </Link>
+          ))
+        )}
       </div>
       <Footer />
 
